d1050ea5a1509dea34abfff70f898210ea02919f
Sebastian Berchtold <Seb.Berchtold@web.de>
2021-01-28 02:44:43 +0100

    Add impressum

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-    </footer>
+  <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
+<a href="impressum">Impressum</a></footer>
 )
 
 Footer.propTypes = {
