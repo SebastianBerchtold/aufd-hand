72426b81e226589cb20a6c61341deac4e73497fd
Sebastian Berchtold <Seb.Berchtold@web.de>
2022-02-12 11:45:58 +0100

    Test speisekarten link

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,31 @@ import schriftzug from '../images/aufdhand-schriftzug.png'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <span className="logo">
-      <img src={schriftzug} width="300" alt="Aufd Hand"/>
+	<img src={schriftzug} width="300" alt="Aufd Hand"/>
     </span>
+      <div style={{margin: "1m 0  0 "}}>
+	  <h2>
+
+	      <a href="http://karte.aufd-hand.de">
+	      <b>
+		  Speis &amp; Trank
+		  &nbsp;&nbsp;
+		  &nbsp;&nbsp;
+		  &nbsp;&nbsp;
+		  &nbsp;&nbsp;
+
+	      </b>
+	  </a>
+
+	      <i class='fa fa-instagram'></i>
+
+	  <a href="https://www.instagram.com/aufdhand.berchtesgaden/?hl=de">
+		  Food Porn
+	  </a>
+	  </h2>
+      </div>
     <div className="content">
-    <div className="inner">
+	<div className="inner">
         <h1> Event Catering <br/> & Schornbad </h1>
         <h2>
 	  Berchtesgaden
