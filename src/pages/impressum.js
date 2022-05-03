4c2103b2271724cf210fe51a0ab17b9feef7c1a0
Sebastian Berchtold <Seb.Berchtold@web.de>
2022-05-03 21:44:28 +0200

    Add Datenschutzerklärung Facebook und Insta

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -36,7 +36,94 @@ Schornstra&szlig;e 7<br />
 
 <p>Quelle: <a href="https://www.e-recht24.de">eRecht24</a></p>
     <Link to="/">Go back to the homepage</Link>
+
+
+
+
+<h2>Datenschutzerklärung für Facebook und Instagramm</h2>
+<p>
+Wir greifen unter anderem auf auf die technische
+Plattform und die Dienste der Meta Ireland Ltd., 4 Grand Canal Square Grand Canal
+Harbour, Dublin 2, Ireland zurück.
+</p>
+
+<p>
+Wir weisen Sie darauf hin, dass Sie usere Facebook/Instagramm-Seite und ihre Funktionen in eigener
+Verantwortung nutzen. Dies gilt insbesondere für die Nutzung der interaktiven Funktionen
+(z.B. Kommentieren, Teilen, Bewerten). Alternativ können Sie die über diese Seite
+angebotenen Informationen auch auf diesem Internet-Angebot abrufen.
+    </p>
+<p>
+Beim Besuch unserer Facebook/Instagramm-Seite erfasst Facebook/Instagramm u.a Ihre IP-Adresse sowie weitere
+Informationen, die in Form von Cookies auf Ihrem PC vorhanden sind. Diese
+Informationen werden verwendet, um uns als Betreiber der Facebook/Instagramm-Seiten statistische
+Informationen über die Inanspruchnahme der Facebook/Instagramm-Seite zur Verfügung zu stellen.
+Nähere Informationen hierzu stellt Facebook/Instagramm unter folgendem Link zur Verfügung:
+http://de-de.facebook.com/help/pages/insights
+    </p>
+<p>
+Diese nutzen wir aber nicht. Wir kochen lieber.
+    </p>
+
+<p>
+Die in diesem Zusammenhang über Sie erhobenen Daten werden von der Meta Ltd.
+verarbeitet und dabei gegebenenfalls in Länder außerhalb der Europäischen Union
+übertragen. Welche Informationen Facebook/Instagramm erhält und wie diese verwendet werden,
+beschreibt Facebook/Instagramm in allgemeiner Form in seinen Datenverwendungsrichtlinien. Dort
+finden Sie auch Informationen über Kontaktmöglichkeiten zu Facebook/Instagramm sowie zu den
+Einstellmöglichkeiten für Werbeanzeigen. Die Datenverwendungsrichtlinien sind unter
+folgendem Link verfügbar:
+http://de-de.facebook.com/about/privacy
+https://help.instagram.com/196883487377501
+Die vollständigen Datenrichtlinien von Facebook finden Sie hier:
+https://de-de.facebook.com/full_data_use_policy
+</p>
+
+
+<p>
+In welcher Weise Facebook/Instagramm die Daten aus dem Besuch von Facebook/Instagramm-Seiten für eigene
+Zwecke verwendet, in welchem Umfang Aktivitäten auf der Facebook/Instagramm-Seite einzelnen
+Nutzern zugeordnet werden, wie lange Facebook/Instagramm diese Daten speichert und ob Daten aus
+einem Besuch der Facebook/Instagramm-Seite an Dritte weitergegeben werden, wird von Facebook/Instagramm
+nicht abschließend und klar benannt und ist uns nicht bekannt.
+Beim Zugriff auf eine Facebook/Instagramm-Seite wird die Ihrem Endgeräte zugeteilte IP-Adresse an
+Facebook/Instagramm übermittelt. Nach Auskunft von Facebook/Instagramm wird diese IP-Adresse anonymisiert
+(bei "deutschen" IP-Adressen). Facebook/Instagramm speichert darüber hinaus Informationen über die
+Endgeräte seiner Nutzer (z.B. im Rahmen der Funktion „Anmeldebenachrichtigung“);
+gegebenenfalls ist Facebook/Instagramm damit eine Zuordnung von IP-Adressen zu einzelnen Nutzern
+    möglich.
+    </p>
+<p>
+Wenn Sie als Nutzerin oder Nutzer aktuell bei Facebook/Instagramm angemeldet sind, befindet sich
+auf Ihrem Endgerät ein Cookie mit Ihrer Facebook/Instagramm-Kennung. Dadurch ist Facebook/Instagramm in der
+Lage nachzuvollziehen, dass Sie diese Seite aufgesucht und wie Sie sie genutzt haben.
+Dies gilt auch für alle anderen Facebook/Instagramm-Seiten. Über in Webseiten eingebundene
+Facebook/Instagramm-Buttons ist es Facebook/Instagramm möglich, Ihre Besuche auf diesen Webseiten Seiten zu
+erfassen und Ihrem Facebook/Instagramm-Profil zuzuordnen. Anhand dieser Daten können Inhalte
+    oder Werbung auf Sie zugeschnitten angeboten werden.
+</p>
+<p>
+Wenn Sie dies vermeiden möchten, sollten Sie sich bei Facebook/Instagramm abmelden bzw. die
+Funktion "angemeldet bleiben" deaktivieren, die auf Ihrem Gerät vorhandenen Cookies
+löschen und Ihren Browser beenden und neu starten. Auf diese Weise werden Facebook/Instagramm-
+    Informationen, über die Sie unmittelbar identifiziert werden können, gelöscht. Damit
+können Sie unsere Facebook/Instagramm-Seite nutzen, ohne dass Ihre Facebook/Instagramm-Kennung offenbart
+wird. Wenn Sie auf interaktive Funktionen der Seite zugreifen (Gefällt mir, Kommentieren,
+Teilen, Nachrichten etc.), erscheint eine Facebook/Instagramm-Anmeldemaske. Nach einer etwaigen
+    Anmeldung sind Sie für Facebook/Instagramm erneut als bestimmte/r Nutzerin/Nutzer erkennbar.
+</p>
+
+<p>
+Informationen dazu, wie Sie über Sie vorhandene Informationen verwalten oder löschen
+können, finden Sie auf folgenden Facebook/Instagramm Support-Seiten:
+https://de-de.facebook.com/about/privacy#
+https://help.instagram.com/196883487377501
+Wir als Anbieter des Informationsdienstes erheben und verarbeiten darüber hinaus keine Daten aus Ihrer Nutzung unseres Dienstes.
+</p>
+<p>Quelle: <a href="https://www.datenschutz.rlp.de/fileadmin/lfdi/Dokumente/Muster_Datenschutzerklaerung_Facebook.pdf">datenschutz rlp</a></p>
+    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
+
 
 export default SecondPage
